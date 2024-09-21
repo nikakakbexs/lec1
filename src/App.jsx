cc63b7584ebe2c3da45fee97ea8a7a3940a0a54b
nikakakbexs <kakabadzenikanika@gmail.com>
2024-09-21 05:52:26 -0700

    teats

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  const firstName = "nika";
+  return (
+    <div>
+      <h1 className="title">hello world</h1>
+    </div>
+  );
+}
+
+export default App;
